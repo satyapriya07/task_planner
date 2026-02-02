fca027cd0e1b6ca585e520e79e83153094b88ef4
satyapriya sahoo <satyapriyasahoo2004@gmail.com>
2026-02-02 17:35:59 +0530

    initialize Angular frontend with routing

---
+++ technician-daily-planner/frontend/src/app/components/today-tasks/today-tasks.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-today-tasks',
+  imports: [],
+  templateUrl: './today-tasks.html',
+  styleUrl: './today-tasks.css',
+})
+export class TodayTasks {
+
+}
